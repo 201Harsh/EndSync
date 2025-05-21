fec7fb076d401c55d20eab67497c8116994424f5
Harsh <gamerpandeyharsh@gmail.com>
2025-05-21 08:44:13 +0530

    logic updated

--- Frontend/src/Pages/Start.jsx
+++ Frontend/src/Pages/Start.jsx
@@ -568,7 +568,7 @@ export default function Start() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 to="/register"
-                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 flex items-center transition-colors"
+                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 flex items-center transition-colors"
               >
                 Get started
                 <ArrowRightCircleIcon className="h-5 w-5 ml-2" />
